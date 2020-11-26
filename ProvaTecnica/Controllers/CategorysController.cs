381ba579432bd4aeb1d2432d2675f665517fc029
Matheus Willian <matheuswillian91@hotmail.com>
2020-11-26 14:58:09 -0300

    categorys criada

---
+++ ProvaTecnica/Controllers/CategorysController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ProvaTecnica.Models;
+
+namespace ProvaTecnica.Controllers
+{
+    public class CategorysController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Category> list = new List<Category>();
+            list.Add(new Category { Id = 1, Name = "Medicamentos", Description = "Remedios" });
+            list.Add(new Category { Id = 2, Name = "Remedios", Description = "Medicamentos" });
+            return View(list);
+        }
+    }
+}
