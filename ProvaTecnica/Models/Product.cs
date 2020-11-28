594acaaae8b1578fa97d3a282b6bd8244d3c6dd3
Matheus Willian <matheuswillian91@hotmail.com>
2020-11-28 00:22:34 -0300

    required retirado

--- ProvaTecnica/Models/Product.cs
+++ ProvaTecnica/Models/Product.cs
@@ -9,28 +9,27 @@ namespace ProvaTecnica.Models
     public class Product
     {
         public int Id { get; set; }
-                
-        [Required]
-        [StringLength(45, MinimumLength = 1, ErrorMessage = "Mínimo {2} e máximo {1} caracteres")]
+
+        //[Required]
+        //[StringLength(45, MinimumLength = 1, ErrorMessage = "Mínimo {2} e máximo {1} caracteres")]
         public string Name { get; set; }
 
-        [Required]
+        //[Required]
         public string Description { get; set; }
 
-        [Required]
-        [Range(0.01, 10000000000000000000.00, ErrorMessage = "Valor precisa ser maior que 0.01")]
+        //[Required]
+        //[Range(0.01, 10000000000000000000.00, ErrorMessage = "Valor precisa ser maior que 0.01")]
         [DisplayFormat(DataFormatString = "{0:F2}")]
         public double Value { get; set; }
 
-        [Required]
-        [StringLength(45, MinimumLength = 1, ErrorMessage = "Mínimo {2} e máximo {1} caracteres")]
+        //[Required]
+        //[StringLength(45, MinimumLength = 1, ErrorMessage = "Mínimo {2} e máximo {1} caracteres")]
         public string Brand { get; set; }
 
-        [Required]
+        //[Required]
         [Display(Name = "Category")]
         public int CategoryId { get; set; }
 
-        [Required]
         public Category Category { get; set; }
 
 
